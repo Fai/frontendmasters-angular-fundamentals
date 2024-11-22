0b58945263c8ec339d3fa382280c0f16e6e13c87
Ruchida <im@fai.in.th>
2024-11-22 09:25:42 +0000

    add solution for project 00 and project 01

--- projects/01-hello-angular/src/app/app.component.ts
+++ projects/01-hello-angular/src/app/app.component.ts
@@ -9,7 +9,16 @@ import { RouterOutlet } from '@angular/router';
   template: `
     <h1>If you are reading this...</h1>
     <p>Things have worked out well! 🎉</p>
+    <ol>
+      <li>My favorite book is <strong>Harry Potter</strong></li>
+      <li>My favorite movie is <strong>My Devil Wears Prada</strong></li>
+      <li>My favorite song is <strong>Big Mini World</strong></li>
+    </ol>
   `,
-  styles: ``,
+  styles: `
+  li {
+    list-style-type: upper-roman;
+  }
+  `,
 })
 export class AppComponent {}
